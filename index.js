3461dd1d41369ad31a57d4439206f4d1a4dd8eac
ahyunlee5 <ahyunlee@umich.edu>
2019-11-20 13:24:10 -0800

    third

--- index.js
+++ index.js
@@ -3,11 +3,20 @@
 function showInfoContent() {
   $('.info').on('click', function() {
     $('.infoContent').toggleClass('hidden');
+    $('.infoLabel').toggleClass('hidden');
+  });
+}
+
+function showProjectsContent() {
+  $('.projects').on('click', function() {
+    $('.projectsContent').toggleClass('hidden');
+    $('.projectsLabel').toggleClass('hidden');
   })
 }
 
 function allFunction() {
   showInfoContent();
+  showProjectsContent();
 }
 
 $(allFunction)
