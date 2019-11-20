813e3a78b25f197604fc79cde9c6e8bc161ff045
ahyunlee5 <ahyunlee@umich.edu>
2019-11-19 23:42:09 -0800

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 'use strict';
 
+function showInfoContent() {
+  $('.info').on('click', function() {
+    $('.infoContent').toggleClass('hidden');
+  })
+}
+
+function allFunction() {
+  showInfoContent();
+}
+
+$(allFunction)
