b2bfa24360721805137eba1c24f3dbe004722eb3
ahyunlee5 <ahyunlee@umich.edu>
2019-11-21 00:20:34 -0800

    fourth

--- index.js
+++ index.js
@@ -11,12 +11,38 @@ function showProjectsContent() {
   $('.projects').on('click', function() {
     $('.projectsContent').toggleClass('hidden');
     $('.projectsLabel').toggleClass('hidden');
-  })
+  });
+}
+
+function showContactContent() {
+  $('.contacts').on('click', function() {
+    $('.contactsContent').toggleClass('hidden');
+    $('.contactsLabel').toggleClass('hidden');
+  });
 }
 
 function allFunction() {
   showInfoContent();
   showProjectsContent();
+  showContactContent();
 }
 
-$(allFunction)
+// function carousel() {
+
+// }
+
+// function plusSlides(n){
+//     clearInterval(myTimer);
+//     if (n < 0){
+//       showSlides(slideIndex -= 1);
+//     } else {
+//      showSlides(slideIndex += 1); 
+//     }
+//     if (n === -1){
+//       myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+//     } else {
+//       myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+//     }
+//   }
+
+$(allFunction);
